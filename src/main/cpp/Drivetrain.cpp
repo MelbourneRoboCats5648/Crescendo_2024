5e9328512e222b431625dd6b23ab6dcf3269eecd
lnoconnor <lnoconnor@gmail.com>
2024-02-17 18:36:40 +1100

    updated so that the drive train contains the 4 drive modules. Each drive module contains a speed motor, direction motor, encoder and its own PID controller

--- src/main/cpp/Drivetrain.cpp
+++ src/main/cpp/Drivetrain.cpp
@@ -3,23 +3,6 @@
 //Encoder Counts Per RV
 const int countsPerRevolution = 4096;
 
-using namespace ctre::phoenix6::hardware;
-
-TalonFX frontLeftSpeedMotor(1,"rio");
-TalonFX frontRightSpeedMotor(2,"rio");
-TalonFX backLeftSpeedMotor(3,"rio");
-TalonFX backRightSpeedMotor(4,"rio");
-TalonFX frontLeftDirectionMotor(5,"rio");
-TalonFX frontRightDirectionMotor(6,"rio");
-TalonFX backLeftDirectionMotor(7,"rio");
-TalonFX backRightDirectionMotor(8,"rio");
-
-//direction encoders
-CANcoder frontLeftDirectionEncoder(9,"rio");
-CANcoder frontRightDirectionEncoder(10,"rio");
-CANcoder backLeftDirectionEncoder(11,"rio");
-CANcoder backRightDirectionEncoder(12,"rio");
-
 // Locations for the swerve drive modules relative to the robot center.
 frc::Translation2d m_frontLeftLocation{0.260825_m, 0.260825_m};
 frc::Translation2d m_frontRightLocation{0.259325_m, -260825_m};
@@ -31,16 +14,16 @@ const frc::SwerveDriveKinematics<4> kinematics{
                                         m_backLeftLocation,
                                         m_backRightLocation};
 
-void DriveTrain::SetAllModules(frc::ChassisSpeeds fieldSpeeds){
+void DriveTrain::SetAllModules(frc::ChassisSpeeds chassisSpeed){
      
     // Convert to module states. Here, we can use C++17's structured
     // bindings feature to automatically split up the array into its
     // individual SwerveModuleState components.
-    auto [fl, fr, bl, br] = kinematics.ToSwerveModuleStates(fieldSpeeds);
-    SetModule(fl, frontLeftSpeedMotor, frontLeftDirectionMotor, frontLeftDirectionEncoder);
-    SetModule(fr, frontRightSpeedMotor, frontRightDirectionMotor, frontRightDirectionEncoder);
-    SetModule(bl, backLeftSpeedMotor, backLeftDirectionMotor, backLeftDirectionEncoder);
-    SetModule(br, backRightSpeedMotor, backRightDirectionMotor, backRightDirectionEncoder);
+    auto [fl, fr, bl, br] = kinematics.ToSwerveModuleStates(chassisSpeed);
+    SetModule(fl, m_frontLeftModule);
+    SetModule(fr, m_frontRightModule);
+    SetModule(bl, m_backLeftModule);
+    SetModule(br, m_backRightModule);
 }
 
 /*
@@ -48,54 +31,27 @@ void DriveTrain::SetAllModules(frc::ChassisSpeeds fieldSpeeds){
  * get motor angle and speed from module state info
  * move each motor
 */
-void DriveTrain::SetModule(frc::SwerveModuleState state, ctre::phoenix6::hardware::TalonFX& driveMotor, ctre::phoenix6::hardware::TalonFX& directionMotor, ctre::phoenix6::hardware::CANcoder& encoder) {
-//reference state
+void DriveTrain::SetModule(frc::SwerveModuleState state, DriveModule& driveModule) {
+  // Setting Motor Speed
+  const units::meters_per_second_t MAX_SPEED_MPS = 32.203_mps;
+  double normalisedSpeed = (double)state.speed / (double)MAX_SPEED_MPS;
+
+  driveModule.m_speedMotor.Set(normalisedSpeed);
+
+  //TODO check encoder range
+  double encoderCurrentState = driveModule.m_directionEncoder.GetAbsolutePosition().GetValueAsDouble();
+  units::angle::radian_t encoderRotation = units::angle::radian_t{encoderCurrentState/(2* M_PI)};
+
+  //optimise
+  frc::SwerveModuleState::Optimize(state, encoderRotation);
+
+  // Calculate the turning motor output from the turning PID controller.
+    const auto turnOutput = driveModule.m_turningPIDController.Calculate(
+      {encoderRotation}, state.angle.Radians());
 
 
-// Setting Motor Speed
-const units::meters_per_second_t MAX_SPEED_MPS = 32.203_mps;
-
-double normalisedSpeed = (double)state.speed / (double)MAX_SPEED_MPS;
-
-driveMotor.Set(normalisedSpeed);
-
-//TODO check encoder range
-double encoderCurrentState = encoder.GetAbsolutePosition().GetValueAsDouble();
-units::angle::radian_t encoderRotation = units::angle::radian_t{encoderCurrentState/(2* M_PI)};
-
-
-//optimise
-frc::SwerveModuleState::Optimize(state, encoderRotation);
-
-//Module constants defining
- static constexpr auto kModuleMaxAngularVelocity =
-      std::numbers::pi * 1_rad_per_s;  // radians per second
- static constexpr auto kModuleMaxAngularAcceleration =
-    std::numbers::pi * 2_rad_per_s / 1_s;  // radians per second^2
-
-//define PID
- frc::ProfiledPIDController<units::radians> m_turningPIDController{
-      1.0,
-      0.0,
-      0.0,
-      {kModuleMaxAngularVelocity, kModuleMaxAngularAcceleration}};
-
-
-// Calculate the turning motor output from the turning PID controller.
-  const auto turnOutput = m_turningPIDController.Calculate(
-    {encoderRotation}, state.angle.Radians());
-
-
-
-// Set the motor outputs for the turning of the wheels
-directionMotor.SetVoltage(units::volt_t{turnOutput}); //+ drFeedforward);
-
-
+  // Set the motor outputs for the turning of the wheels
+  driveModule.m_directionMotor.SetVoltage(units::volt_t{turnOutput}); //+ drFeedforward);
 
 };
-
-// Find out current angle of direction motor from encoder
-// Find out which direction to rotate direction motor
-// Drive the angle motor
-// Find out when to stop (maybe need to check if you are ate the required angle)
 
--- src/main/include/DriveTrain.h
+++ src/main/include/DriveTrain.h
@@ -8,12 +8,47 @@
 #include <frc/controller/ProfiledPIDController.h>
 #include <frc/motorcontrol/PWMSparkMax.h>
 
+//Module constants defining
+ static constexpr auto kModuleMaxAngularVelocity =
+      std::numbers::pi * 1_rad_per_s;  // radians per second
+ static constexpr auto kModuleMaxAngularAcceleration =
+    std::numbers::pi * 2_rad_per_s / 1_s;  // radians per second^2
 
+using namespace ctre::phoenix6::hardware;
+
+class DriveModule{
+public:
+    DriveModule(int speedMotorID, int directionMotorID, int directionEncoderID) :
+        m_speedMotor(speedMotorID, "rio"),
+        m_directionMotor(directionMotorID, "rio"),
+        m_directionEncoder(directionEncoderID, "rio"),
+        m_turningPIDController{
+            1.0,
+            0.0,
+            0.0,
+            {kModuleMaxAngularVelocity, kModuleMaxAngularAcceleration}}
+    {
+        // Reset PID controller on startup
+    }
+
+public:
+    TalonFX m_speedMotor;
+    TalonFX m_directionMotor;
+    CANcoder m_directionEncoder;
+    frc::ProfiledPIDController<units::radians> m_turningPIDController;
+};
 
 class DriveTrain{
 public:
-    void SetAllModules(frc::ChassisSpeeds state);
-private:
-    void SetModule(frc::SwerveModuleState state, ctre::phoenix6::hardware::TalonFX& driveMotor, ctre::phoenix6::hardware::TalonFX& directionMotor, ctre::phoenix6::hardware::CANcoder& encoder);
+    void SetAllModules(frc::ChassisSpeeds chasisSpeed);
 
+private:
+    void SetModule(frc::SwerveModuleState state, DriveModule& driveModule);
+
+private:
+    // Todo - update these hard coded id numbers to constants
+    DriveModule m_frontLeftModule{1, 5, 9};
+    DriveModule m_frontRightModule{2, 6, 10};
+    DriveModule m_backLeftModule{3, 7, 11};
+    DriveModule m_backRightModule{4, 5, 12};
 };
