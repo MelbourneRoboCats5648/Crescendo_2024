7850ba58ec8457a221f7ef69268ad73d364ec3f7
lnoconnor <lnoconnor@gmail.com>
2024-03-17 10:47:55 +1100

    Finished...think it works...needtotest and double check

--- src/main/cpp/Shoot+Intake.cpp
+++ src/main/cpp/Shoot+Intake.cpp
@@ -8,12 +8,14 @@ void ShootAndIntake(frc::XboxController& xbox,
             frc::VictorSP& motorShooterLeft,
             frc::VictorSP& motorShooterRight)
 {
-auto seconds = shooterTimer.Get();
 
-if (true == xbox.GetYButton())
+
+if (true == xbox.GetYButtonPressed())
 {
     shooterTimer.Reset();
+    auto seconds = shooterTimer.Get();
     shooterTimer.Start();
+    
     motorShooterLeft.Set(speakerShooterSpeed);
     motorShooterRight.Set(-1.0*speakerShooterSpeed);
     if (seconds<5_s && seconds>3_s)
@@ -24,6 +26,30 @@ if (true == xbox.GetYButton())
     {
         motorIntakeWheel.Set(0);
     }
+}
+
+else if (true == xbox.GetAButtonPressed())
+{
+    shooterTimer.Reset();
+    auto seconds = shooterTimer.Get();
+    shooterTimer.Start();
+    
+    motorShooterLeft.Set(ampShooterSpeed);
+    motorShooterRight.Set(-1.0*ampShooterSpeed);
+    if (seconds<5_s && seconds>3_s)
+    {
+        motorIntakeWheel.Set(speakerIntakeWheelOutSpeed);
+    }
+    else
+    {
+        motorIntakeWheel.Set(0);
+    }
+}
+
+else {
+    motorShooterLeft.Set(0);
+    motorShooterRight.Set(0);
+    motorIntakeWheel.Set(0);
 }
 
 }
