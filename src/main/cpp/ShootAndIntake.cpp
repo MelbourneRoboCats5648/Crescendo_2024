d3b6c3733381dab03c4ae524fc799df3d2f90bfc
lnoconnor <lnoconnor@gmail.com>
2024-06-16 15:03:16 +1000

    Fixed the shooter intake button so all buttons work

--- src/main/cpp/ShootAndIntake.cpp
+++ src/main/cpp/ShootAndIntake.cpp
@@ -13,28 +13,37 @@ void ShootAndIntake::ShootAndIntakeFunctions(frc::XboxController& xbox)
         shooterTimer.Reset();
         shooterTimer.Start();
     }
-
+    
     auto milliseconds = shooterTimer.Get();
 
-    if(milliseconds < 4_s && milliseconds > 0_s)
+    if (milliseconds > 0_s)
     {
-        m_shooter.motorShooterLeft.Set(speakerShooterSpeed);
-        m_shooter.motorShooterRight.Set(-1.0*speakerShooterSpeed);
-    }
-    else
-    {
-        m_shooter.motorShooterLeft.Set(0.0);
-        m_shooter.motorShooterRight.Set(0.0);
-        //TODO - maybe stop and reset timer here???
-    }
+        if(milliseconds < 4_s && milliseconds > 0_s)
+        {
+            m_shooter.motorShooterLeft.Set(speakerShooterSpeed);
+            m_shooter.motorShooterRight.Set(-1.0*speakerShooterSpeed);
+        }
+        else
+        {
+            m_shooter.motorShooterLeft.Set(0.0);
+            m_shooter.motorShooterRight.Set(0.0);
+            //TODO - maybe stop and reset timer here???
+        }
 
-    if(milliseconds < 4_s && milliseconds > 2_s)
-    { 
-        m_intake.motorIntakeWheel.Set(speakerIntakeWheelOutSpeed);
-    }
-    else
-    {
-        m_intake.motorIntakeWheel.Set(0.0);
+        if(milliseconds < 4_s && milliseconds > 2_s)
+        { 
+            m_intake.motorIntakeWheel.Set(speakerIntakeWheelOutSpeed);
+        }
+        else
+        {
+            m_intake.motorIntakeWheel.Set(0.0);
+        }
+
+        if(milliseconds > 4_s)
+        {
+            shooterTimer.Stop();
+            shooterTimer.Reset();
+        }
     }
 
 }
