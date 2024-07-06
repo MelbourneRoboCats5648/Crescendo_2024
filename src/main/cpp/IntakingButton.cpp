f696ade68686ab214ecac403a00b7599879e0162
lnoconnor <lnoconnor@gmail.com>
2024-07-06 17:00:04 +1000

    increased deadband on the joystick rotation and reducing speed of the intake and move button

--- src/main/cpp/IntakingButton.cpp
+++ src/main/cpp/IntakingButton.cpp
@@ -5,7 +5,7 @@
 void IntakingButton(ShootAndIntake& shootAndIntake, 
                     DriveTrain& driveTrain)
 {
-    frc::ChassisSpeeds speeds{1.0_mps, 0.0_mps, units::radians_per_second_t(0)};
+    frc::ChassisSpeeds speeds{0.5_mps, 0.0_mps, units::radians_per_second_t(0)};
     shootAndIntake.m_intake.motorIntakeWheel.Set(intakeWheelInSpeed);
     driveTrain.SetAllModules(speeds);
-}
+} 
